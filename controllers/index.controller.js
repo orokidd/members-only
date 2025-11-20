409568495e92ca3f956b03410725e765d0356715
orokidd <gregomily@gmail.com>
2025-11-20 13:39:06 +0800

    show database data in index

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,7 +3,7 @@ const db = require("../models/queries")
 const controller = {
     getIndex: async (req, res) => {
         const allPosts = await db.getAllPosts()
-        res.render("index", { user: req.user , posts: allPosts})
+        res.render("index", { user: req.user , posts: allPosts}) // Passport attaches user object to req.user
     }
 }
 
