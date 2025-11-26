b1426c30d87ea54db70340c729a6d26436d1b860
orokidd <gregomily@gmail.com>
2025-11-26 15:43:47 +0800

    profile ejs and styles

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,6 +3,7 @@ const db = require("../models/queries")
 const controller = {
     getIndex: async (req, res) => {
         const allPosts = await db.getAllPosts()
+        
         res.render("index", { user: req.user , posts: allPosts}) // Passport attaches user object to req.user
     }
 }
