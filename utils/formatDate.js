449d872f3694303dedcdf7a345e0145c4b79a709
orokidd <gregomily@gmail.com>
2025-12-02 17:52:26 +0800

    post date format change, delete and edit post button css fixes

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,7 +1,7 @@
 const { format } = require("date-fns")
 
 function formatDate(dateString) {
-    return format(new Date(dateString), 'MMM dd, yyyy, hh:mm a')
+    return format(new Date(dateString), 'MMM dd, yyyy, HH:mm')
 }
 
 module.exports = formatDate
