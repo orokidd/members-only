75c2f4fa5ba079b793339a25c41b1d72d4956986
orokidd <gregomily@gmail.com>
2025-12-17 12:03:41 +0800

    change pool to local db (render expired)

--- models/pool.js
+++ models/pool.js
@@ -1,10 +1,19 @@
 const { Pool } = require("pg");
 
+// Pool setup for online hosting
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+})
 
 module.exports = pool;
